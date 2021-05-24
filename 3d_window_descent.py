e012ee865970fdbe6011646908058800aa2d3211
WyattSP <69209835+WyattSP@users.noreply.github.com>
2021-05-24 13:30:28 -0600

    v0.0.1
    Command line interface functions work. Brief read.me file created.

--- 3d_window_descent.py
+++ 3d_window_descent.py
@@ -5,28 +5,13 @@ Created on Sat May 22 12:44:17 2021
 
 @author: wyattpetryshen
 """
+import fire
 import matplotlib.pyplot as plt
 from random import randrange
 import numpy as np
-
-#Create square 3D surface
-def square_surface(array_size):
-    N = array_size
-    # the x and y coordinate of the center point
-    center_arr = (N - 1) / 2
-    # the coordinates of the square array
-    row, col = np.ogrid[:N, :N]
-    # N - distance gives the result
-    new_surface = N/2 - np.maximum(np.abs(row - center_arr), np.abs(col - center_arr))
-    for i in np.arange(1,new_surface.shape[1]-1):
-        for j in range(new_surface.shape[0]):
-            if new_surface[j,i] == 0:
-                new_surface[j,i] = 0
-            elif new_surface[j,i] >= 0:
-                new_surface[j,i] = -new_surface[j,i]
-            elif new_surface[j,i] <= 0:
-                new_surface[j,i] = new_surface[j,i]
-    return new_surface
+import plotly.graph_objects as go
+import plotly
+import os
 
 #Boundary array function
 def min_dims(row_val,col_val,w_size,arr):
@@ -144,38 +129,73 @@ def gradient_descent_3d(array,x_start,y_start,steps=10,step_size=1,plot=True):
             break
     return step_vals,step_history
 
-#Iterate through many runs
-def iter_movement(runs,array,steps=10,step_size=1,plot_all=True):
-    # Initial point to start gradient descent at
-    array = array
-    steps = steps
-    step_size = step_size
-    runs = runs
-    color=iter(plt.cm.rainbow(np.linspace(0,1,runs)))
-    #Initiates plot
-    if plot_all:
-            fig, ax = plt.subplots()
-            CS = ax.contour(np.arange(0,array.shape[0]), np.arange(0,array.shape[1]),array,levels = 25, linewidths = 0.25)
-            ax.clabel(CS, inline=True, fontsize=5)
-            ax.set_title('Adaptive Surface')
-    #Iterates through runs
-    for k in range(runs):
-            c = next(color)
-            x_start = randrange(0,array.shape[0]-1)
-            y_start = randrange(0,array.shape[1]-1)
-            vals_t, step_t = gradient_descent_3d(array,x_start,y_start,steps=steps,step_size=step_size,plot=False)
-            x_steps = [i[0] for i in step_t]
-            y_steps = [i[1] for i in step_t]
-            next_x = x_steps[0]
-            next_y = y_steps[0]
-            plt.plot(next_y,next_x,'ro',c=c, marker = "d")
-            for i in np.arange(1,len(x_steps)-1):
-                past_x = next_x
-                past_y = next_y
-                next_x = x_steps[i]
-                next_y = y_steps[i]
-                plt.plot([past_y,next_y],[past_x,next_x],'k-', c='black')
-                plt.plot(next_y,next_x,'ro', c=c, marker = "o")
+class Core_Functions(object):
+    
+    #Create square 3D surface
+    def square_surface(self, array_size = 1000, plot = False):
+        N = array_size
+        # the x and y coordinate of the center point
+        center_arr = (N - 1) / 2
+        # the coordinates of the square array
+        row, col = np.ogrid[:N, :N]
+        # N - distance gives the result
+        new_surface = N/2 - np.maximum(np.abs(row - center_arr), np.abs(col - center_arr))
+        for i in np.arange(1,new_surface.shape[1]-1):
+            for j in range(new_surface.shape[0]):
+                if new_surface[j,i] == 0:
+                    new_surface[j,i] = 0
+                elif new_surface[j,i] >= 0:
+                    new_surface[j,i] = -new_surface[j,i]
+                elif new_surface[j,i] <= 0:
+                    new_surface[j,i] = new_surface[j,i]
+        if plot:
+            sh_0, sh_1 = new_surface.shape
+            x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+
+            topo = go.Figure(data=[go.Surface(z=new_surface, x=x, y=y)])
+            topo.update_traces(contours_z=dict(show=True, usecolormap=True,
+                                  highlightcolor="limegreen", project_z=True))
+            plotly.offline.plot(topo)
+        np.save('surface.npy',new_surface)
+        return new_surface
+    
+    #Iterate through many runs
+    def iter_movement(self, runs, steps=10,step_size=1,plot_all=True):
+        # Initial point to start gradient descent at
+        path  = os.getcwd()
+        array = np.load(path + '/surface.npy')
+        steps = steps
+        step_size = step_size
+        runs = runs
+        color=iter(plt.cm.rainbow(np.linspace(0,1,runs)))
+        #Initiates plot
+        if plot_all:
+                fig, ax = plt.subplots()
+                CS = ax.contour(np.arange(0,array.shape[0]), np.arange(0,array.shape[1]),array,levels = 25, linewidths = 0.25)
+                ax.clabel(CS, inline=True, fontsize=5)
+                ax.set_title('Adaptive Surface')
+        #Iterates through runs
+        for k in range(runs):
+                c = next(color)
+                x_start = randrange(0,array.shape[0]-1)
+                y_start = randrange(0,array.shape[1]-1)
+                vals_t, step_t = gradient_descent_3d(array,x_start,y_start,steps=steps,step_size=step_size,plot=False)
+                x_steps = [i[0] for i in step_t]
+                y_steps = [i[1] for i in step_t]
+                next_x = x_steps[0]
+                next_y = y_steps[0]
+                plt.plot(next_y,next_x,'ro',c=c, marker = "d")
+                for i in np.arange(1,len(x_steps)-1):
+                    past_x = next_x
+                    past_y = next_y
+                    next_x = x_steps[i]
+                    next_y = y_steps[i]
+                    plt.plot([past_y,next_y],[past_x,next_x],'k-', c='black')
+                    plt.plot(next_y,next_x,'ro', c=c, marker = "o")
+        plt.savefig('evolution_surface.png')
+        return print('All finished!')
                 
-                
+if __name__ == '__main__':
+    core_functions = Core_Functions()
+    fire.Fire(core_functions)           
                                 
--- README.md
+++ README.md
@@ -3,10 +3,56 @@ Models of evolution towards adaptive optimums using a simple sliding window impl
 
 Each major version release will add a layer of complexity to the models. 
 
-##Current Release
+## Current Release
 
 * v 0.0.1
 
 Simple gradient descent implemented using a sliding window
 
-Main function: iter_movement()
+Model requires the usuage of two functions. The first creates the surface to run gradient descent, the second is the actual implementation.
+Provided is an example with the required code.
+
+To run the model:
+
+1) Open a new command window
+
+
+2) Set working directory to folder containing 3d_sliding_descent.py
+
+
+3) Create your surface
+
+'''
+python 3d_window_descent.py square_surface
+'''
+
+Input parameters for square_surface:
+
+    array_size: Length of square array to create. Default set to 1000.
+    
+    plot: Plot surface using plotly
+    
+
+4) Run the model
+
+'''
+python 3d_window_descent.py iter_movement 25 20 10
+'''
+
+Input parameters for square_surface:
+
+    runs: Number of individuals moving across the adaptive surface.
+    
+    steps: The number of steps each individual will take. Default set to 10.
+    
+    step_size: The distance of each step. Default set to 1.
+    
+    plot_all: Default True. Unstable if set to False
+    
+
+Outputs are saved into your working directory as individual files.
+
+* square surface returns surface.npy
+* iter_movement returns evolution_surface.png 
+
+surface.npy must exist in your working directory for iter_movement to run.
